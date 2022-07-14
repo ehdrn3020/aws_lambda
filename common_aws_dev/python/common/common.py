2f9a1fb3c569fcae3cd88e1d257ba114fec41f3a
ehdrn3020 <ehdrnsla3020@naver.com>
2022-07-14 14:58:08 +0900

    dev module추가

---
+++ common_aws_dev/python/common/common.py
@@ -0,0 +1,6 @@
+DB = 'mydb_dev'
+TABLE = 'mytable_dev'
+BUCEKT = 'my-bucket-dev'
+ATHENA_OUTPUT_LOCATION = 's3://my-bucket/athena/'
+SNS_TOPIC_ARN = "arn:aws:sns:ap-northeast-2:567567567567:lambda_project"
+ES_URL = "https://elk.url.dev.com:8100/myelk-{}/_doc/{}"
